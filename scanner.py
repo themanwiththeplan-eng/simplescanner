79199e0129d93985732e3026732e37f0ec7f654a
themanwiththeplan-eng <dakotadunn5@gmail.com>
2022-04-15 13:07:32 -0700

    simple port scanner tool

---
+++ scanner.py
@@ -0,0 +1,36 @@
+#!/bin/pyton3
+
+import sys 
+import socket as sck
+from datetime import datetime as dt
+
+#Define our target
+if len(sys.argv) == 2:
+        target = sck.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+else:
+        print("Invalid amount of arguments")
+        print("Syntax: python3 scanner.py <ip>")
+
+#Add a pretty banner
+print("-" * 50)
+print("Scanning target " + target)
+print("Time started: " + str(dt.now()))
+
+try: 
+        for port in range(1,65535):
+                s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+                sck.setdefaulttimeout(1)
+                result = s.connect_ex((target, port)) #returns an error indicator 
+                if result == 0:
+                        print("Port {} is open".format(port))
+                s.close()
+except KeyboardInterrupt:
+        print("\n Exiting program.")
+        sys.exit()
+except sck.gaierror:
+        print("Hostname could not be resolved")
+        sys.exit()
+except sck.error:
+        print("Couldn't connect to server.")
+        sys.exit()
+h
